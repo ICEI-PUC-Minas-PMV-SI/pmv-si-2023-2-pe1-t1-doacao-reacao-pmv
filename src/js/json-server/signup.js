36f0f2a96e7b7854be8d9e4495fff25bb954149d
vcrfrancisco <vcrfrancisco@sga.pucminas.br>
2023-11-25 11:35:08 -0300

    feat: add login-signup

---
+++ src/js/json-server/signup.js
@@ -0,0 +1,52 @@
+const signupForm = document.getElementById("signup-form")
+const mailErrorMsg = document.getElementById("mail-error-msg")
+
+const loggedOngPage = "./home-logged.html"
+const accountsURL= 'http://localhost:3001/accounts'
+
+signupForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const email = signupForm.email.value;
+    const response = await fetch(accountsURL)
+    const accounts = await response.json()
+
+    const foundUser = accounts.find(account => account.mail === email)
+
+    if (foundUser){
+        return mailErrorMsg.style.opacity = 1;
+    }
+
+    const password = signupForm.senha.value;
+    const confirmPassword = signupForm.confirmarSenha.value;
+
+ if (password !== confirmPassword) {
+        return passwordErrorMsg.style.opacity = 1;
+    }
+
+    const firstName = signupForm.firstname.value;
+    const lastName = signupForm.lastname.value;
+    console.log({
+        id: accounts.length + 1,
+        mail: email,
+        password: password,
+        firstName: firstName,
+        lastName: lastName
+    })
+    fetch(
+        accountsURL,
+        {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+            method: 'POST',
+            body: JSON.stringify({
+                id: accounts.length + 1,
+                mail: email,
+                password: password,
+                firstName: firstName,
+                lastName: lastName
+            }),
+            }).then(postResponse => postResponse.json()).then(postResponse => console.log(postResponse))
+    location.replace(loggedOngPage);
+        })
